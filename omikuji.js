b8b476d1f38c899d26534f119c1bc535f189de7b
yuma133 <kinbaraff@gmail.com>
2020-06-07 17:31:24 +0900

    Initial Commit Message

---
+++ omikuji.js
@@ -0,0 +1,32 @@
+var name;
+var username;
+var userresult;
+ 
+username = prompt("お名前を教えて下さい。");
+ 
+document.getElementById("name").innerHTML = username;
+
+if(document.getElementById("name").innerHTML == ""){
+ 	
+ 	document.getElementById("name").innerHTML = "名無し";
+ 	
+ }
+
+var rand = Math.floor( Math.random() * 5); 
+ 
+if (rand == 0) {
+	userresult = "大吉";
+}
+if (rand == 1) {
+	userresult = "中吉";
+}
+if (rand == 2) {
+	userresult = "小吉";
+}
+if (rand == 3) {
+	userresult = "吉";
+}
+if (rand == 4) {
+	userresult = "凶";
+}
+document.getElementById("result").innerHTML = userresult;
